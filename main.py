7467fff26661e1ab1d4ba1de5b913b6f8dacb101
ariana <ariana.moreno13@yahoo.com>
2023-07-14 19:18:52 -0600

    updating file to pylint standards

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
-#Write a program named check_month that prompts a user to enter a birth month. 
+#Write an if-else program prompts a user to enter a birth month.  
 #If the value entered is greater than 12 or less than 1, display an error message; otherwise, display the valid month
-
-#Ex:
-
-#if the user enter 3,  you display "March"
-#if the user enter 5,  you display "May"
-#if the user enter 12,  you display "December"
-#... do the same with all valid months. i.e. January, February, March, etc...
- 
-
-#if the user enters 13 or greater,  you display "ERROR"
-#if the user enter 0 or smaller,  you display "ERROR"
- 
-
-
-birthMonth = int(input("Please enter the month you were born."))
-if birthMonth == 1:
-    month = "January" 
-elif birthMonth == 2:
-    month = "Febuary"
-elif birthMonth == 3:
-    month = "March"
-elif birthMonth == 4:
-    month = "April"
-elif birthMonth == 5:
-    month = "May"
-elif birthMonth == 6:
-    month = "June"
-elif birthMonth == 7:
-    month = "July"
-elif birthMonth == 8:
-    month = "August"
-elif birthMonth == 9:
-    month = "September"
-elif birthMonth == 10:
-    month = "October"
-elif birthMonth == 11:
-    month = "November"
-elif birthMonth == 12:
-    month = "December"
-#elif birthMonth >= 0 <= 13:
-    #month = "Invalid month"
+BIRTH_MONTH = int(input("Please enter the month you were born."))
+if BIRTH_MONTH == 1:
+    MONTH = "January"
+elif BIRTH_MONTH == 2:
+    MONTH = "Febuary"
+elif BIRTH_MONTH == 3:
+    MONTH = "March"
+elif BIRTH_MONTH == 4:
+    MONTH = "April"
+elif BIRTH_MONTH == 5:
+    MONTH = "May"
+elif BIRTH_MONTH == 6:
+    MONTH = "June"
+elif BIRTH_MONTH == 7:
+    MONTH = "July"
+elif BIRTH_MONTH == 8:
+    MONTH = "August"
+elif BIRTH_MONTH == 9:
+    MONTH = "September"
+elif BIRTH_MONTH == 10:
+    MONTH = "October"
+elif BIRTH_MONTH == 11:
+    MONTH = "November"
+elif BIRTH_MONTH == 12:
+    MONTH = "December"
 else:
     print("Birth month must be a number from 1-12")
-
- 
